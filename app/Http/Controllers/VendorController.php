f6775bb92edc9dd5f0576cc11cefbfa58d4e98fb
kelvin muchikha <kmuchikha@gmail.com>
2024-10-29 07:09:45 +0300

    v.c

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vendor; // Ensure the Vendor model is imported
 use Illuminate\Http\Request;
 
 class VendorController extends Controller
 {
-    //
+    // Display a listing of vendors
+    public function index()
+    {
+        // Fetch all vendors from the database
+        $vendors = Vendor::all();
+
+        // Return the index view with vendors data
+        return view('vendors.index', compact('vendors'));
+    }
+
+    // Other methods (create, store, edit, update, destroy) go here
 }
