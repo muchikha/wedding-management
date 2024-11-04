e9fa93b47e8f48f27a8777753ab6321f53087261
kelvin muchikha <kmuchikha@gmail.com>
2024-11-04 22:45:52 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title', 'My Application')</title>
     <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}"> <!-- Updated to use Tailwind CSS -->
 </head>
-<body class="bg-gray-100"> <!-- Optional: Added a background color for better aesthetics -->
+<body class="bg-gray-100"> <!-- Added a background color for better aesthetics -->
     <div class="container mx-auto p-4"> <!-- Center the container and add some padding -->
         @yield('content')
     </div>
